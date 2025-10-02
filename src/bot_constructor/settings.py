b33ee0476b06c5ae17984bde3d308aa2497d11d7
Wiltort <wiltort21@gmail.com>
2025-10-02 12:42:54 +0500

    update settings

--- src/bot_constructor/settings.py
+++ src/bot_constructor/settings.py
@@ -56,6 +56,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://89.104.71.118",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "http://web:8000",
+    "http://0.0.0.0",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
